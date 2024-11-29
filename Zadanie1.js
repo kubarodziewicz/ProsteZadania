37accf8cfc0c2970fd6873b30cf0f7d0b7372647
Kuba <skibidiziemniak@skebede.com>
2024-11-29 09:19:33 +0100

    1st commit

---
+++ Zadanie1.js
@@ -0,0 +1,17 @@
+
+const processText = (text, { uppercase = false, reverse = false, removeSpecialChars = false } = {}) =>{
+
+    
+    
+   
+   } 
+   
+
+var opcje = {
+    uppercase: true,
+    reverse: true,
+    removeSpecialChars: true
+};
+
+
+processText("skibidi", opcje)
